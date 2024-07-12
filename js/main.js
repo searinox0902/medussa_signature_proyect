d38cd7d16ea37189767a834e5b9193c43a0c9947
sebastianvargas@esinergia.co <sebastian.vargas@esinergia.co>
2024-07-12 15:40:47 -0500

    Add UX dev: qrpopup, pdfviewver, signature, facedet, documento, allcomponents

---
+++ js/main.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const snap = document.getElementById("snap");
+const errorMsgElement = document.querySelector('span#errorMsg');
+
+const constraints = {
+  audio: true,
+  video: {
+    width: 320, height: 390
+  }
+};
+
+// Access webcam
+async function init() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+  } catch (e) {
+    errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+  }
+}
+
+// Success
+function handleSuccess(stream) {
+  window.stream = stream;
+  video.srcObject = stream;
+}
+
+// Load init
+init();
+
+// Draw image
+var context = canvas.getContext('2d');
+/*
+snap.addEventListener("click", function() {
+        context.drawImage(video, 0, 0,   490, 320);
+        canvas.style.display = "block"
+});*/
